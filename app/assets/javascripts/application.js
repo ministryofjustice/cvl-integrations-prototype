afe100f3d97826810c682339a55441f566d79920
samrwright <sam.wright@digital.justice.gov.uk>
2024-11-25 16:31:18 +0000

    Added more approver journey stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,18 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+  window.onload = function () {
+    // Loops through dom and finds all elements with card--clickable class
+    document.querySelectorAll('.card--clickable').forEach(card => {
+      // Check if card has a link within it
+      if (card.querySelector('a') !== null) {
+        // Clicks the link within the heading to navigate to desired page
+        card.addEventListener('click', () => {
+          card.querySelector('a').click()
+        })
+      }
+    })
+  }
 })
+
+
