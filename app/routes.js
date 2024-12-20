45c0ce2149a72ddb759c968b6aab51fee8ab9bb5
samrwright <sam.wright@digital.justice.gov.uk>
2024-12-20 10:52:19 +0000

    PP updates

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,36 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+router.post('/are-you-sure-answer', function(request, response) {
+
+    var areyousure = request.session.data['createLicence']
+    if (areyousure == "yes"){
+        response.redirect("/probation-practitioner/who-with")
+    } else {
+        response.redirect("/probation-practitioner/case-list")
+    }
+})
+
+
+router.post('/additional-conditions-answer', function(request, response) {
+
+    var addConditions = request.session.data['additionalConditions']
+    if (addConditions == "yes"){
+        response.redirect("/../../../_common/not-testing.html")
+    } else {
+        response.redirect("/probation-practitioner/bespoke-conditions")
+    }
+})
+
+router.post('/bespoke-conditions-answer', function(request, response) {
+
+    var bespConditions = request.session.data['bespokeConditions']
+    if (bespConditions == "yes"){
+        response.redirect("/../../../_common/not-testing.html")
+    } else {
+        response.redirect("/probation-practitioner/check-licence")
+    }
+})
+
