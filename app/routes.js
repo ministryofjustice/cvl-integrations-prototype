7871a10b8c467b3325a088503fe7a8a3d092e50d
samrwright <sam.wright@digital.justice.gov.uk>
2025-01-28 11:10:38 +0000

    mvp2 updates

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,7 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+// MVP1
 
 router.post('/are-you-sure-answer', function(request, response) {
 
@@ -40,3 +41,37 @@ router.post('/bespoke-conditions-answer', function(request, response) {
     }
 })
 
+
+// MVP2
+
+
+router.post('/are-you-sure-answer2', function(request, response) {
+
+    var areyousure = request.session.data['createLicence']
+    if (areyousure == "yes"){
+        response.redirect("/mvp2/probation-practitioner/pre-release/create/who-with")
+    } else {
+        response.redirect("/mvp2/probation-practitioner/pre-release/create/case-list")
+    }
+})
+
+
+router.post('/additional-conditions-answer2', function(request, response) {
+
+    var addConditions = request.session.data['additionalConditions']
+    if (addConditions == "yes"){
+        response.redirect("/../../../_common/not-testing.html")
+    } else {
+        response.redirect("/mvp2/probation-practitioner/pre-release/create/bespoke-conditions")
+    }
+})
+
+router.post('/bespoke-conditions-answer2', function(request, response) {
+
+    var bespConditions = request.session.data['bespokeConditions']
+    if (bespConditions == "yes"){
+        response.redirect("/../../../_common/not-testing.html")
+    } else {
+        response.redirect("/mvp2/probation-practitioner/pre-release/create/check-licence")
+    }
+})
